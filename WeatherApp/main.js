4b94d52a9549b99b81a77fbba71698a8613fa351
DavidKurzFH <2310653633@stud.fh-kufstein.ac.at>
2025-05-26 23:19:02 +0200

    finished project

--- WeatherApp/main.js
+++ WeatherApp/main.js
@@ -1,12 +1,29 @@
-import { state } from "./state.js"
-import { showWeather, showError } from "./ui.js"
-// c1dc84d8bfce46b29a160501250804
+import { setWeatherData, getWeatherData } from "./state.js";
+import { updateWeatherUI } from "./ui.js";
 
-document.getElementById("loadBtn").addEventListener("click", async () => {
-    const input = document.getElementById("input")
+const API_KEY = "c1dc84d8bfce46b29a160501250804"
 
-    if (input === ''){
-        const error = "Empty query"
-        showError(error)
+document.getElementById("fetchWeatherBtn").addEventListener("click", async () => {
+  const city = document.getElementById("cityInput").value.trim();
+
+  if (!city) {
+    alert("Bitte gib eine Stadt ein.");
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&lang=de`
+    );
+
+    if (!response.ok) {
+      throw new Error("Stadt nicht gefunden.");
     }
-})
+    
+    const data = await response.json();
+    setWeatherData(data);
+    updateWeatherUI(getWeatherData());
+  } catch (error) {
+    alert("Fehler beim Abrufen der Wetterdaten: " + error.message);
+  }
+});
--- WeatherApp/ui.js
+++ WeatherApp/ui.js
@@ -1,10 +1,24 @@
-export function showWeather(weather) {
-    // NUR für das Rendern zuständig, kein API Call
-    const output = document.getElementById("result")
-    output.innerHTML = "Loading..."
-}
+export function updateWeatherUI(data) {
+  const cityNameEl = document.getElementById("cityName");
+  const tempEl = document.getElementById("temperature");
+  const descEl = document.getElementById("description");
+  const iconEl = document.getElementById("weatherIcon");
+  const displayEl = document.getElementById("weatherDisplay");
 
-export function showError(msg){
-    // NUR das ANZEIGEN von Fehler
-    const output = document
+  if (!data) {
+    cityNameEl.textContent = "Keine Daten vorhanden.";
+    tempEl.textContent = "";
+    descEl.textContent = "";
+    iconEl.src = "";
+    return;
+  }
+
+  console.log("!");
+
+  cityNameEl.textContent = data.location.name;
+  tempEl.textContent = `Temperatur: ${data.current.temp_c} °C`;
+  descEl.textContent = data.current.condition.text;
+  iconEl.src = `https://${data.current.condition.icon}`;
+
+  displayEl.classList.remove("d-none");
 }
